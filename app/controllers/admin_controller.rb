a7ee451ab3a90c6a8797e2ce3d51238048bcf64c
Diego Rubin <rubin.diego@gmail.com>
2016-01-02 10:35:00 -0200

    fix admin layout.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,15 @@ class AdminController < ApplicationController
   before_action :is_admin?
 
   protected
+  def set_layout
+    'admin'
+  end
+
   def is_admin?
     unless current_user.admin?
       redirect_to root_url
       return
     end
   end
+
 end
