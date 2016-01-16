60a7af16f09532ad7e2ef200c8f1b46c3705b9ba
Diego Rubin <rubin.diego@gmail.com>
2016-01-16 17:18:41 -0200

    fix jpeg orientation.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,34 +2,19 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or ImageScience support:
-  include CarrierWave::RMagick
-  # include CarrierWave::ImageScience
+  include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
   permissions 0666
-  # storage :s3
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "assets/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
    def default_url
     ActionController::Base.helpers.image_path("fallback/" + [version_name, "image.png"].compact.join('_'))
    end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fill => [100, 100]
   end
@@ -42,10 +27,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def fix_exif_rotation
+    manipulate! do |img|
+      img.tap(&:auto_orient)
+    end
+  end
+
+  process :fix_exif_rotation
 
 end
 
